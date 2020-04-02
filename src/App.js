fc05fa170883eb782660858829aa43502ad963a7
unknown <andriescuioana@gmail.com>
2020-04-02 17:41:52 +0200

    base structure and styling

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Today from './Today/Today'
+import History from './History/History'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="">
+        <div className="topheader">
+          <header className="container">
+            <nav className="navbar">
+              <div className="navbar-brand">
+                <span className="navbar-item">AsteroidAlert</span>
+              </div>
+              <div className="navbar-end">
+                <a className="navbar-item" href="https://api.nasa.gov/" target="_blank" rel="noopener noreferrer">Powered by NASA</a>
+              </div>
+            </nav>
+          </header>
+        </div>
+        <section className="results--section">
+          <div className="container">
+            <h1>AsteroidAlert provides realtime information about near Earth asteroids</h1>
+          </div>
+          <div className="results--section__inner">
+            <Today />
+            <History />
+          </div>
+        </section>
+      </div>
+    );
+  }
 }
 
 export default App;
