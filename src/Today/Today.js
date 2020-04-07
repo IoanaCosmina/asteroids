da6e18e1d3dba7bb77a105817defe1b5195cdde2
unknown <andriescuioana@gmail.com>
2020-04-07 17:49:33 +0200

    add Pagination component

--- src/Today/Today.js
+++ src/Today/Today.js
@@ -4,13 +4,12 @@ import axios from 'axios';
 import * as Utils from '../utils.js';
 import * as Constants from '../constants.js';
 import Asteroid from '../Asteroid/Asteroid';
+import Pagination from '../Pagination/Pagination';
 
 class Today extends Component {
     constructor() {
         super();
-        this.state = {
-            asteroids: []
-        };
+        this.state = { allAsteroids: [], currentAsteroids: [], currentPage: null, totalPages: null };
     }
 
     componentDidMount() {
@@ -18,25 +17,49 @@ class Today extends Component {
         axios.get(`${Constants.API_URL}/feed?start_date=${current_date}&end_date=${current_date}&api_key=${Constants.API_KEY}`)
             .then(response => {
                 const asteroidObj = response.data.near_earth_objects;
-                this.setState({ asteroids: asteroidObj[Object.keys(asteroidObj)[0]] });
-                console.log("asteroids", this.state.asteroids);
+                this.setState({ allAsteroids: asteroidObj[Object.keys(asteroidObj)[0]] });
             })
             .catch(error => {
                 console.log(error);
-            })
+            });
+    }
+
+    onPageChanged = data => {
+        const { allAsteroids } = this.state;
+        const { currentPage, totalPages, pageLimit } = data;
+
+        const offset = (currentPage - 1) * pageLimit;
+        const currentAsteroids = allAsteroids.slice(offset, offset + pageLimit);
+
+        this.setState({ currentPage, currentAsteroids, totalPages });
     }
 
     render() {
+        const {
+            allAsteroids,
+            currentAsteroids
+        } = this.state;
+        const totalAsteroids = allAsteroids.length;
+
+        if (totalAsteroids === 0) return null;
+
         return (
             <div>
                 <h1 className="title is-spaced">Today - {Utils.getCurrentDate("string")}</h1>
-                <h2 className="subtitle">{this.state.asteroids.length} Asteroids</h2>
+                <h2 className="subtitle">{totalAsteroids} Asteroids</h2>
+                <div className="">
+                    <Pagination
+                        totalRecords={totalAsteroids}
+                        pageLimit={3}
+                        pageNeighbours={1}
+                        onPageChanged={this.onPageChanged}
+                    />
+                </div>
                 <div className="container today--container">
-                    {
-                        this.state.asteroids.map(asteroid => (
-                            <Asteroid key={asteroid.name} asteroid={asteroid} />
-                        ))
-                    }
+                    {currentAsteroids.map(asteroid => (
+                        // Replace name with ID inside key
+                        <Asteroid key={asteroid.name} asteroid={asteroid} />
+                    ))}
                 </div>
             </div>
         )
