06606ebe36acca75d5436e55749df794da309d7f
unknown <andriescuioana@gmail.com>
2020-04-16 10:08:47 +0200

    Updates

---
+++ precache-manifest.c53b9bc4ee7e99a8f53ad9843ebd23f5.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12798721988bf9166592c1f91225a8d9",
+    "url": "/asteroids/index.html"
+  },
+  {
+    "revision": "4e73c4302891712446f6",
+    "url": "/asteroids/static/css/main.5f65c9f5.chunk.css"
+  },
+  {
+    "revision": "bf4d9fb09c54dac22678",
+    "url": "/asteroids/static/js/2.8231a0ab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/asteroids/static/js/2.8231a0ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e73c4302891712446f6",
+    "url": "/asteroids/static/js/main.6d5617e3.chunk.js"
+  },
+  {
+    "revision": "e071033cbd649e82f4d8",
+    "url": "/asteroids/static/js/runtime-main.c9e7ac1f.js"
+  },
+  {
+    "revision": "73bb763c208476b94731674ad2d217e5",
+    "url": "/asteroids/static/media/1.73bb763c.jpg"
+  },
+  {
+    "revision": "d247ff0d05c0c9154e8db2328ebc9e51",
+    "url": "/asteroids/static/media/2.d247ff0d.jpg"
+  },
+  {
+    "revision": "e26c2309a180cfdecbcb17380c2ea19e",
+    "url": "/asteroids/static/media/3.e26c2309.jpg"
+  },
+  {
+    "revision": "bfc9319404be44865b72394f65f9eb9f",
+    "url": "/asteroids/static/media/4.bfc93194.jpg"
+  },
+  {
+    "revision": "08fdadb9a704f6e11e5acec19c30148e",
+    "url": "/asteroids/static/media/5.08fdadb9.jpg"
+  }
+]);
--- src/constants.js
+++
@@ -1,2 +0,0 @@
-export const API_URL = "https://api.nasa.gov/neo/rest/v1";
-export const API_KEY = "DEMO_KEY"; //replace with your key
